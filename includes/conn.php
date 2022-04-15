9811df0787f513c81a27f3c267fbca2d565b1970
ManhCoder1201 <manhgaubeo0609@gmail.com>
2022-04-15 17:51:34 +0700

    sua conn2

--- includes/conn.php
+++ includes/conn.php
@@ -1,18 +1,31 @@
 <?php
-    $severname = "3.132.234.157";
-    $username = "manh48";
-    $password = "123@123";
-    $database = "ecomm";
-    //Khai báo biến để kết nỗi CSDL
-    $connect = new mysqli($severname,$username,$password,$database);
-    //$connect = new mysqli_connect ----> Hướng Thủ Tục
-    if(!$connect)
-        {
-            echo "Connect Failed!";
-        }
-        else
-        {
-            
-        }
 
+Class Database{
+ 
+	private $server = "3.132.234.157";
+	private $username = "manh48";
+	private $password = "123@123";
+	private $database = "ecomm";
+	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+	protected $conn;
+ 	
+	public function open(){
+ 		try{
+ 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
+ 			return $this->conn;
+ 		}
+ 		catch (PDOException $e){
+ 			echo "There is some problem in connection: " . $e->getMessage();
+ 		}
+ 
+    }
+ 
+	public function close(){
+   		$this->conn = null;
+ 	}
+ 
+}
+
+$pdo = new Database();
+ 
 ?>
